7baf18593c71a43087a7b7404ed16904b3fa042f
crnicavic <cavic5000@tutanota.com>
2026-04-06 08:17:22 +0200

    obsidian.el bootstrap

--- init.el
+++ init.el
@@ -196,3 +196,29 @@
        :config
        ;;literate
        (default +bindings +smartparens))
+
+
+(use-package obsidian
+  :config
+  (global-obsidian-mode t)
+  (obsidian-backlinks-mode t)
+  :custom
+  ;; location of obsidian vault
+  (obsidian-directory "/mnt/c/Users/ognjen.cavic/Documents/homovault/")
+  ;; Default location for new notes from `obsidian-capture'
+  (obsidian-inbox-directory "slipbox")
+  ;; Useful if you're going to be using wiki links
+  (markdown-enable-wiki-links t)
+
+  ;; These bindings are only suggestions; it's okay to use other bindings
+  :bind (:map obsidian-mode-map
+              ;; Create note
+              ("C-c C-n" . obsidian-capture)
+              ;; If you prefer you can use `obsidian-insert-wikilink'
+              ("C-c C-l" . obsidian-insert-link)
+              ;; Open file pointed to by link at point
+              ("C-c C-o" . obsidian-follow-link-at-point)
+              ;; Open a different note from vault
+              ("C-c C-p" . obsidian-jump)
+              ;; Follow a backlink for the current file
+              ("C-c C-b" . obsidian-backlink-jump)))
